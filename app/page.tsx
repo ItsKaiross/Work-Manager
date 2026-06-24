8d2ac3b20e79cbbaa1f467b9f901fd066cf7997d
Itsharuki <markjunellex@gmail.com>
2026-06-24 12:27:43 -0400

    Beta 0.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,50 @@
+"use client";
 import Image from "next/image";
+import React, { useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+    <div className="min-h-screen flex items-center justify-center">
+      <main>
+        {/* Login Box */}
+        <div className="flex flex-col w-110 rounded-lg bg-gray-100 shadow-lg p-15">
+          <h1 className="text-black self-center text-3xl">Log in</h1>
+          <input className="bg-white shadow-md rounded-md px-4 py-1 mt-5"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="juandelacruz@gmail.com"
+          required
+          />
+          <input className="bg-white shadow-md rounded-md px-4 py-1 mt-5" 
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="********"
+          required 
+          />
+          <button className="flex w-full px-4 py-2 bg-gray-200 mt-5 justify-center rounded-lg text-black hover:bg-blue-500 hover:text-white transition duration-300">
+            Log in
+          </button>
+          <Link className="flex w-full justify-center mt-1 bottom-0 text-sm text-gray-400" href={""}>forgot password?</Link>
+          
+          <div className="flex items-center mt-5">
+          <div className="flex-1 border-t border-gray-400"></div>
+          <p className="text-sm mx-4 text-gray-400">or</p>
+          <div className="flex-1 border-t border-gray-400"></div>
+          </div>
+          <div className="w-full self-center mt-5">
+            <button className="flex w-full bg-gray-200 px-4 py-2 rounded-lg text-black hover:bg-blue-500 hover:text-white transition duration-300 justify-center">
+              <img className="w-5 h-5 mr-5" src="/images/google.png" alt="" /> Continue with Google
+            </button>
+          </div>
+          <div className="flex w-full justify-center mt-2">
+          <p>New user?</p><Link className="ml-2" href={""}>Sign up</Link>
+          </div>
+          
         </div>
       </main>
     </div>
